520e4b0f6fe69affc45fbc079e70dbdcc3d7edb7
kimjinhong2023 <kimjinhong01@naver.com>
2025-02-20 03:33:10 +0900

    Feat: 미니 게임 추가
    FlappyPlane, TheStack 에셋 추가

---
+++ Assets/FlappyPlane/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace FlappyPlane
+{
+    public class GameManager : MonoBehaviour
+    {
+        static GameManager gameManager;
+        public static GameManager instance { get { return gameManager; } }
+
+        private int currentScore = 0;
+
+        UIManager uiManager;
+
+        public UIManager UIManager { get { return uiManager; } }
+
+        private void Awake()
+        {
+            gameManager = this;
+            uiManager = FindObjectOfType<UIManager>();
+        }
+
+        private void Start()
+        {
+            uiManager.UpdateScore(0);
+        }
+
+        public void GameOver()
+        {
+            Debug.Log("Game Over");
+            uiManager.SetRestart();
+        }
+
+        public void RestartGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        public void AddScore(int score)
+        {
+            currentScore += score;
+            Debug.Log("Score: " + currentScore);
+            uiManager.UpdateScore(currentScore);
+        }
+    }
+}
